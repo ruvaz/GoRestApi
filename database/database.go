6b662702f62716402cecd19d00c81b1b3c1ea25f
ruben.vazquez <Ruben.Vazquez@dieboldnixdorf.com>
2020-07-12 22:24:19 -0500

    DB added

---
+++ database/database.go
@@ -0,0 +1,18 @@
+package database
+
+import (
+	"database/sql"
+)
+
+func InitDb() *sql.DB {
+	connectionString := "root@tcp(localhost:3306)/nortwind"
+
+	databaseConnection, error := sql.Open("mysql", connectionString)
+	defer databaseConnection.Close() //se va a cerrar cuando se dejede usar
+
+	if error != nil {
+		panic(error.Error()) //manejo de errores
+	}
+
+	return databaseConnection
+}
--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"net/http"
+	"GoRestApi/database"
+	"fmt"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+// "net/http"
+// "github.com/go-chi/chi"
+// "github.com/go-chi/chi/middleware"
+
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
-	})
-	http.ListenAndServe(":3000", r)
+	databaseConn := database.InitDb()
+	defer databaseConn.Close() //se va a cerrar cuando se dejede usar
+
+	fmt.Println(databaseConn)
+	// r := chi.NewRouter()
+	// r.Use(middleware.Logger)
+	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("welcome"))
+	// })
+	// http.ListenAndServe(":3000", r)
 }
