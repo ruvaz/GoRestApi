aac3359aa933e49d5c440442e8e42ff3ef73cb5d
ruben.vazquez <Ruben.Vazquez@dieboldnixdorf.com>
2020-07-28 10:36:59 -0500

    go mysql drive

--- database/database.go
+++ database/database.go
@@ -8,6 +8,7 @@ func InitDb() *sql.DB {
 	connectionString := "root@tcp(localhost:3306)/nortwind"
 
 	databaseConnection, error := sql.Open("mysql", connectionString)
+
 	defer databaseConnection.Close() //se va a cerrar cuando se dejede usar
 
 	if error != nil {
--- main.go
+++ main.go
@@ -2,24 +2,64 @@ package main
 
 import (
 	"GoRestApi/database"
+	"database/sql"
+	"encoding/json"
 	"fmt"
-
+	"github.com/go-chi/chi"
 	_ "github.com/go-sql-driver/mysql"
+	"net/http"
 )
 
-// "net/http"
-// "github.com/go-chi/chi"
-// "github.com/go-chi/chi/middleware"
+//global
+var databaseConn *sql.DB
+
+type Product struct {
+	ID           int    `json:"id"`
+	Product_Code string `json:"product_code"`
+	Description  string `json:"description"`
+}
 
 func main() {
-	databaseConn := database.InitDb()
+	databaseConn = database.InitDb()
 	defer databaseConn.Close() //se va a cerrar cuando se dejede usar
 
 	fmt.Println(databaseConn)
-	// r := chi.NewRouter()
-	// r.Use(middleware.Logger)
-	// r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Write([]byte("welcome"))
-	// })
-	// http.ListenAndServe(":3000", r)
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+
+	r.Get("/products", AllProductos)
+	http.ListenAndServe(":3000", r)
+}
+
+func respWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _:= json.Marshal(payload)
+
+	fmt.Println(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+func catch(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func AllProductos(w http.ResponseWriter, r *http.Request) {
+	const sql = `Select id, product_code,COALESCE(description,'') 
+ 					from products`
+	result, err := databaseConn.Query(sql)
+	catch(err)
+	var products []*Product
+	for result.Next() {
+		product := &Product{}
+		err = result.Scan(&product.ID, &product.Product_Code, &product.Description)
+		catch(err)
+		products = append(products, product)
+	}
+	respWithJSON(w, http.StatusOK, products)
 }
